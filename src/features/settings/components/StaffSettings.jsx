7c56bddaf9d31c491efd49898092116915b55577
ponzi <ponzialcubo@gmail.com>
2026-04-06 18:05:21 +0200

    Seccion ajustes añadir equipo x4

--- src/features/settings/components/StaffSettings.jsx
+++ src/features/settings/components/StaffSettings.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { supabase } from '../../../../config/supabase';
+import { supabase } from '../../../config/supabase';
 import AddEmployeeModal from './AddEmployeeModal';
 
 export default function StaffSettings() {
