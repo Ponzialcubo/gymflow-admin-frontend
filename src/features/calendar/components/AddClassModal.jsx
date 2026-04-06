9f5986cbb3be7e6d791cec063a71ff5ad2434bbb
ponzi <ponzialcubo@gmail.com>
2026-04-06 18:27:35 +0200

    Añadido calendario x2

--- src/features/calendar/components/AddClassModal.jsx
+++ src/features/calendar/components/AddClassModal.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { supabase } from '../../../../config/supabase';
+import { supabase } from '../../../config/supabase';
 
 export default function AddClassModal({ isOpen, onClose, onClassAdded, classToEdit }) {
   const [monitores, setMonitores] = useState([]);
